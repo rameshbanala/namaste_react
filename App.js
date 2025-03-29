b90b937fbb59c45dc8c25d807f73fbd775223810
ramesh <rameshbanalab@gmail.com>
2025-03-29 15:55:09 +0530

    ignited_app

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
-let heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from react"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/**
+ *
+ * <div id="parent">
+ *    <div id="child">
+ *        <h1 id="heading">Hello world from react</h1>
+ *    </div>
+ * </div>
+ */
+
+let parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading" }, "Hello world from react")
+  )
 );
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading);
-root.render(heading);
+root.render(parent);
