df25aad0097871688dfb8da837c3e976745521aa
ramesh <rameshbanalab@gmail.com>
2025-03-30 15:07:02 +0530

    about jsx

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", null, "Hello World");
 
-const jsxHeading = <h2 id="heading">Hello this is jsx heading 🚀</h2>
+//react element
+const jsxHeading = <h2 id="heading">Hello this is jsx heading 🚀</h2>;
+
+const HeadingComponent = () => {
+  return <h1>Hello this is functional component</h1>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
